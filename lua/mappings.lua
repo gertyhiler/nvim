02214ef7122f82bf12ef38ef703e914b0e23ef87
Andrew Korobka <tryuserrus@gmail.com>
2024-12-15 22:27:50 +0300

    feat: updated mapping - Change behavior for paste, delete - Added feature moving rows - Delete duplicate hop settings - Change mapping for increment and decrement - Added mapping for go to next diagnostic trouble

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,18 +1,33 @@
 require "nvchad.mappings"
 
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>")
 map({ "n", "i" }, "<C-s>", "<cmd>w<CR>", { desc = "Save" })
 map("n", "<A-s>", "<cmd>wa<CR>", { desc = "Save All" })
 map("n", "<leader>qq", "<cmd>qa<CR>", { desc = "Quit" })
+map("v", "p", '"_dP', opts)
+map("n", "dd", '"_dd', opts)
+map("n", "dr", "dd", opts)
+
 map("n", "<leader>cx", function()
   require("nvchad.tabufline").closeAllBufs()
 end, { desc = "Close All Buffers" })
 map("i", "<C-g>", function()
   return vim.fn["codeium#Accept"]()
 end, { expr = true })
+
+-- move text up and down
+map("v", "J", ":m .+1<CR>==", opts)
+map("v", "K", ":m .-2<CR>==", opts)
+map("x", "J", ":move '>+1<CR>gv-gv", opts)
+map("x", "K", ":move '<-2<CR>gv-gv", opts)
+
+-- better indent handling
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
 
 -- Terminal
 map("n", "<C-]>", function()
@@ -34,23 +49,9 @@ map("t", "<C-f>", function()
   require("nvchad.term").toggle { pos = "float" }
 end, { desc = "Toogle Terminal Float" })
 
--- hop
-
-local hop = require "hop"
-local directions = require("hop.hint").HintDirection
-
-map("", "f", function()
-  hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true }
-end, { remap = true })
-map("", "F", function()
-  hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true }
-end, { remap = true })
-map("", "t", function()
-  hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }
-end, { remap = true })
-map("", "T", function()
-  hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }
-end, { remap = true })
+-- Increment/decrement
+map("n", "+", "<C-a>")
+map("n", "-", "<C-x>")
 
 -- GIT
 map("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "Open LazyGit" })
@@ -67,9 +68,15 @@ end, { desc = "Create Console log" })
 -- Hop
 local hop = require "hop"
 local directions = require("hop.hint").HintDirection
+
 vim.keymap.set("n", "s", function()
   hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = false }
 end, { remap = true })
 vim.keymap.set("n", "S", function()
   hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = false }
 end, { remap = true })
+
+-- Diagnostics
+map("n", "<leader>dn", function()
+	vim.diagnostic.goto_next()
+end, { desc = "Next Diagnostic", silent = true, noremap = true })
